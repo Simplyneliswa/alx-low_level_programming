1801c2883ccbcfa16bb78c259e3d039231e209e5
Simplyneliswa <neliswamaduma@gmail.com>
2023-02-18 10:01:44 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d; 
+	float f;
+	i
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
