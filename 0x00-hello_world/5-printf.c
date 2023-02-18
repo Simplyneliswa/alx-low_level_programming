bf942ff2156362b47cf8be2f900f3bb8e7af021a
Simplyneliswa <neliswamaduma@gmail.com>
2023-02-18 09:15:19 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * * Description: main - Prints out a string to stdout.
+ * * Return: 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is piece of art,\n");return (0);
+}
